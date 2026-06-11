06f2e0aa4f9c680661de1d1ddf915c15f6ab036e
Songsak <phungaudomsongsakdi@gmail.com>
2026-06-11 11:26:52 +0700

    แก้ไขแสดงผลสถานะปกติแทนหน่วยคงเหลือ

--- js/components/stock.js
+++ js/components/stock.js
@@ -18,7 +18,7 @@ const StockComponent = {
                   <th>ราคา(฿)</th>
                   <th>สต็อก</th>
                   <th>สถานะ</th>
-                  <th>📦เซฟตี้สต๊อก</th>
+                  <th>เซฟตี้สต๊อก</th>
                   <th>จัดการ</th>
                 </tr>
               </thead>
